d0f71e29cf6ff9698e9618c1d67268e04848a3c0
saachiaggarwal7 <saachiaggarwal7@gmail.com>
2026-07-09 18:59:24 +0530

    add CloudTrail Scanner

---
+++ Modules/cloudtrail_scanner.py
@@ -0,0 +1,78 @@
+import boto3
+
+def cloudtrail_scanner(session):
+    findings=[]
+    region=session.region_name
+    cloudtrail_client=session.client("cloudtrail")
+    response=cloudtrail_client.describe_trails()
+    trails=response['trailList']
+
+    #detects if CloudTrail is not enabled
+    if not trails:
+        finding = {
+            "rule_id": "CG-CT-001",
+            "service": "CloudTrail",
+            "region": region,
+            "resource": "AWS Account",
+            "severity": "HIGH",
+            "finding": "CloudTrail is not configured.",
+            "recommendation": "Create and enable a CloudTrail trail to record AWS API activity."
+        }
+        findings.append(finding)
+    else:
+        for trail in trails:
+
+            #checking if multi region trail is enabled or not
+            if not trail["IsMultiRegionTrail"]:
+                finding = {
+                    "rule_id": "CG-CT-002",
+                    "service": "CloudTrail",
+                    "region": region,
+                    "resource": trail["Name"],
+                    "severity": "MEDIUM",
+                    "finding": "CloudTrail is not configured as a multi-region trail.",
+                    "recommendation": "Enable multi-region logging to capture API activity across all AWS regions."
+                }
+                findings.append(finding)
+
+            #checks if log file validation is enabled
+            if not trail.get('LogFileValidationEnabled'):
+                finding = {
+                    "rule_id": "CG-CT-003",
+                    "service": "CloudTrail",
+                    "region": region,
+                    "resource": trail["Name"],
+                    "severity": "MEDIUM",
+                    "finding": "CloudTrail log file validation is disabled.",
+                    "recommendation": "Enable log file validation to detect unauthorized modification of CloudTrail logs."
+                }
+                findings.append(finding)
+
+            # checks cloudwatch logs integration
+            if not trail.get("CloudWatchLogsLogGroupArn"):
+                finding = {
+                    "rule_id": "CG-CT-004",
+                    "service": "CloudTrail",
+                    "region": region,
+                    "resource": trail["Name"],
+                    "severity": "MEDIUM",
+                    "finding": "CloudTrail is not integrated with CloudWatch Logs.",
+                    "recommendation": "Integrate CloudTrail with CloudWatch Logs to enable real-time monitoring, alerting, and log analysis."
+                }
+                findings.append(finding)
+            
+            #checks  Customer-managed KMS encryption 
+            if not trail.get("KmsKeyId"):
+                finding = {
+                    "rule_id": "CG-CT-005",
+                    "service": "CloudTrail",
+                    "region": region,
+                    "resource": trail["Name"],
+                    "severity": "MEDIUM",
+                    "finding": "CloudTrail is not encrypted with a customer-managed KMS key.",
+                    "recommendation": "Configure a customer-managed AWS KMS key to encrypt CloudTrail logs."
+                }
+                findings.append(finding)
+    if not findings:
+        print("No CloudTrail vulnerability found.")
+    return findings
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from Modules.s3_scanner import s3_scanner
 from Modules.security_group_scanner import security_group_scanner
 from Modules.report_generator import generate_report
 from Modules.vpc_scanner import vpc_scanner
+from Modules.cloudtrail_scanner import cloudtrail_scanner
+
 import boto3
 
 findings=[]
@@ -59,7 +61,7 @@ while(True):
     print("="*60)
     print(" "*20+"CLOUD GUARD")
     print("="*60)
-    print("\n1. Scan Everything \n2. Scan IAM \n3. Scan S3 \n4. Scan EC2 \n5. Scan Security Groups \n6. VPC Scanner \n7. Generate Report \n8. Exit")
+    print("\n1. Scan Everything \n2. Scan IAM \n3. Scan S3 \n4. Scan EC2 \n5. Scan Security Groups \n6. VPC Scanner \n7. CloudTrail Scanner \n8. Generate Report \n9. Exit")
     try:
         choice=int(input("Enter choice:"))
     except ValueError:
@@ -114,14 +116,21 @@ while(True):
         for session in sessions:
             findings.extend(vpc_scanner(session))
         display_finding(findings)
-
     elif choice==7:
+        findings=[]
+        sessions=create_session()
+        if sessions is None:
+            continue
+        for session in sessions:
+            findings.extend(cloudtrail_scanner(session))
+        display_finding(findings)
+    elif choice==8:
         if findings:
             generate_report(findings)
             print("Report generated successfully!")
         else:
             print("No scan results available. Please run a scan first.")
-    elif choice==8:
+    elif choice==9:
         print("Thank you for using CloudGuard!")
         break
     else:
